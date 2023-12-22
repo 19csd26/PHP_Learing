da1752c4eff8e1233487c2c9336da69cca649b45
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2023-12-22 19:01:45 +0530

    Add files via upload

---
+++ MagicConstants.php
@@ -0,0 +1,128 @@
+<?php
+    //ClassName::class
+    namespace Technical_Portal;  
+    echo "<h3>Example for CLASSNAME::CLASS </h3>";  
+    // Define a class named 'javatpoint' within the 'Technical_Portal' namespace
+    class javatpoint {}  
+    // Output the fully-qualified class name as a string using the ::class syntax
+    echo javatpoint::class;    // ClassName::class 
+    echo "<hr>";
+
+    //__LINE__
+    echo"<h3>Example of __LINE__</h3>";
+    //print Your current line number
+    echo"You are at Line Number:".__LINE__."<br><br>";
+    echo"<hr>";
+
+    //__FILE__
+    echo"<h3>Example of __FILE__</h3>";
+    //print full path of file with .php extension
+    echo __FILE__."<br><br>";
+    echo"<hr>";
+
+    //__DIR__
+    echo"<h3>Example of __DIR__</h3>";
+    //print full path of directory where script will be placed
+    echo __DIR__."<br><br>";
+    //equivalent to above
+    echo dirname(__FILE__)."<br><br>";
+    echo"<hr>"; 
+    
+    //__FUNCTION__
+    echo "<h3>Example for __FUNCTION__</h3>";    
+    //Using magic constant inside function.    
+    function test(){    
+        //print the function name i.e; test.   
+        echo 'The function name is '. __FUNCTION__ . "<br><br>";   
+    }    
+    test();    
+    //Magic constant used outside function gives the blank output.    
+    function test_function(){    
+        echo 'Hie';    
+    }    
+    test_function();    
+    //give the blank output.   
+    echo  __FUNCTION__ . "<br><br>";  
+    echo"<hr>";
+
+    //__CLASS__
+    echo "<h3>Example for __CLASS__</h3>";    
+    class JTP    
+    {    
+        public function __construct() {    
+            ;    
+    }    
+    function getClassName(){    
+        //print name of the class JTP.   
+        echo __CLASS__ . "<br><br>";   
+        }    
+    }    
+    $t = new JTP;    
+    $t->getClassName();    
+      
+    //in case of multiple classes   
+    class base  
+    {    
+    function test_first(){    
+            //will always print parent class which is base here.    
+            echo __CLASS__;   
+        }    
+    }    
+    class child extends base    
+    {    
+        public function __construct() {    
+            ;    
+        }    
+    }    
+    $t = new child;    
+    $t->test_first();    
+    echo"<hr>";
+
+    //__TRAIT__
+    echo "<h3>Example for __TRAIT__</h3>";    
+    trait created_trait {    
+        function jtp(){    
+            //will print name of the trait i.e; created_trait    
+            echo __TRAIT__;  
+        }    
+    }    
+    class Company {    
+        use created_trait;    
+        }    
+    $a = new Company;    
+    $a->jtp();    
+    echo"<hr>";
+
+    //__METHOD__
+    echo "<h3>Example for __METHOD__</h3>";  
+    class method {    
+        public function __construct() {    
+            //print method::__construct    
+                echo __METHOD__ . "<br><br>";   
+            }    
+        public function meth_fun(){    
+            //print method::meth_fun    
+                echo __METHOD__;   
+        }    
+    }    
+    $a = new method;    
+    $a->meth_fun();  
+    echo"<hr>";
+
+    //__NAMESPACE__ 
+    echo "<h3>Example for __NAMESPACE__</h3>";  
+
+    // Define a class named 'name' within the current namespace
+    class name {    
+        public function __construct() {    
+            echo 'This line will print on calling namespace.';     
+        }     
+    }    
+
+    // Create a fully-qualified class name by appending the current namespace to 'name'
+    $class_name = __NAMESPACE__ . '\name';    
+
+    // Instantiate an object of the class using the fully-qualified name
+    $a = new $class_name;   
+    echo "<hr>";
+?>
